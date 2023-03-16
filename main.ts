380251067afbac77ddcdac65ea85655fd89dccf4
MATESATORRE <127900201+MATESATORRE@users.noreply.github.com>
2023-03-16 08:09:28 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ let TERMINO_A = 0
 let TERMINO_B = 0
 let TERMINO_C = 0
 input.onPinPressed(TouchPin.P0, function () {
-    basic.showString("Hola, vamos a resolver la ecuación: ")
+    basic.showString("ECUACION")
     basic.showNumber(TERMINO_A)
     basic.showString("X")
     basic.showString(" + ")
@@ -18,7 +18,7 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(TERMINO_A)
 })
 input.onGesture(Gesture.Shake, function () {
-    basic.showString("LA SOLUCIÓN ES X = ")
+    basic.showString("SOLUCION X= ")
     basic.showNumber((TERMINO_C - TERMINO_B) / TERMINO_A)
 })
 input.onButtonPressed(Button.AB, function () {
@@ -28,4 +28,13 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     TERMINO_B += randint(0, 20)
     basic.showNumber(TERMINO_B)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showString("ECUACION")
+    basic.showNumber(TERMINO_A)
+    basic.showString("X")
+    basic.showString("+")
+    basic.showNumber(TERMINO_B)
+    basic.showString("=")
+    basic.showNumber(TERMINO_C)
 })
